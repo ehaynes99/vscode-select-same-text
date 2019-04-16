163b7c5b9cbd055ca5a58df481f59ea2a71af161
Eric Haynes <ehaynes99@gmail.com>
2019-04-15 22:26:15 -0400

    Initial working add selection function

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Change Log
 
-All notable changes to the "case-sensitive-add-selection" extension will be documented in this file.
+All notable changes to the "case-sensitive-add-to-selection" extension will be documented in this file.
 
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
--- src/extension.js
+++ src/extension.js
@@ -2,6 +2,24 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
+const addSelection = (rowStart, colStart, rowEnd, colEnd) => {
+  const editor = vscode.window.activeTextEditor;
+  const start = new vscode.Position(rowStart, colStart);
+  const end = new vscode.Position(rowEnd, colEnd);
+  editor.selections = [
+    ...editor.selections,
+    new vscode.Selection(start, end),
+  ];
+};
+
+const addNext = () => {
+  const editor = vscode.window.activeTextEditor;
+  if (editor) {
+    addSelection(2, 6, 2, 9);
+    console.log('***** editor.selections:', editor.selections);
+  }
+};
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -9,22 +27,29 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "case-sensitive-add-to-selection" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "case-sensitive-add-selection" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    'extension.caseSensitiveAddSelectionToNextFindMatch',
+    function() {
+      // The code you place here will be executed every time your command is executed
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+      try {
+        addNext();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  );
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
@@ -32,6 +57,6 @@ exports.activate = activate;
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
